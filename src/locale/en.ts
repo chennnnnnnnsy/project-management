f10a03b5d5ba92956ed67cd2657344895dfd13a0
chensy <code.chensy.lin@outlook.com>
2022-06-25 15:16:03 +0800

    chore: 调整国际化的一些结构

--- src/locale/en.ts
+++ src/locale/en.ts
@@ -1,40 +1,34 @@
-const notFound: INotFoundLT = {
-  message: "The page is not found!",
-  backHome: "Back to home",
-};
-const home: IHomeLT = {
-  title: "Better manage your projects !",
-};
-const login: ILoginLT = {
-  title: "Go inside and start managing your project",
-  userName: "Please input username or phone",
-  password: "Please input password",
-  loginBtn: "Login",
-  backBtn: "Back to home",
-  registerBtn: "Please to register",
-  passwordMinLength: "The password length cannot be less than 6 digits",
-};
-const HeaderComp: IHeaderCompLT = {
-  login: "Login",
-  logout: "Logout",
-};
-const profile: IProfileLT = {
-  groupName: "Project Group",
-  start: "Start", // 立项 -- 意味没有定下一个版本之前
-  develop: "Develop", // 迭代中 -- 确定版本号，开始开发了
-  maintaining: "Maintaining", // 维护中 -- 已有版本开发完，且没有开发下一个版本功能前
-  end: "End", // 终止 -- 不再迭代和不再维护
-  status: "Status",
-  version: 'Version',
-  addGroup: 'Add Project Group'
-};
-
 const EN: ILocaleText = {
-  notFound,
-  home,
-  login,
-  HeaderComp,
-  profile,
+  notFound: {
+    message: "The page is not found!",
+    backHome: "Back to home",
+  },
+  home: {
+    title: "Better manage your projects !",
+  },
+  login: {
+    title: "Go inside and start managing your project",
+    userName: "Please input username or phone",
+    password: "Please input password",
+    loginBtn: "Login",
+    backBtn: "Back to home",
+    registerBtn: "Please to register",
+    passwordMinLength: "The password length cannot be less than 6 digits",
+  },
+  profile: {
+    groupName: "Project Group",
+    start: "Start", // 立项 -- 意味没有定下一个版本之前
+    develop: "Develop", // 迭代中 -- 确定版本号，开始开发了
+    maintaining: "Maintaining", // 维护中 -- 已有版本开发完，且没有开发下一个版本功能前
+    end: "End", // 终止 -- 不再迭代和不再维护
+    status: "Status",
+    version: "Version",
+    addGroup: "Add Project Group",
+  },
+  HeaderComp: {
+    login: "Login",
+    logout: "Logout",
+  },
 };
 
 export default EN;
--- src/locale/zh-cn.ts
+++ src/locale/zh-cn.ts
@@ -1,39 +1,34 @@
-const notFound: INotFoundLT = {
-  message: "没有找到该页面！",
-  backHome: "返回首页",
-};
-const home: IHomeLT = {
-  title: "更好地管理您的项目！",
-};
-const login: ILoginLT = {
-  title: "从此进入，开始管理您的项目",
-  userName: "请输入用户名/手机号码",
-  password: "请输入密码",
-  loginBtn: "登录",
-  backBtn: "返回首页",
-  registerBtn: "先去注册",
-  passwordMinLength: "密码长度不能小于6位",
-};
-const HeaderComp: IHeaderCompLT = {
-  login: "登录",
-  logout: "退出登录",
-};
-const profile: IProfileLT = {
-  groupName: "项目组",
-  start: "立项", // 立项 -- 意味没有定下一个版本之前
-  develop: "迭代中", // 迭代中 -- 确定版本号，开始开发了
-  maintaining: "维护中", // 维护中 -- 已有版本开发完，且没有开发下一个版本功能前
-  end: "终止", // 终止 -- 不再迭代和不再维护
-  status: "状态",
-  version: '版本号',
-  addGroup: '新增项目组'
-};
 const ZhCN: ILocaleText = {
-  notFound,
-  home,
-  login,
-  HeaderComp,
-  profile,
+  notFound: {
+    message: "没有找到该页面！",
+    backHome: "返回首页",
+  },
+  home: {
+    title: "更好地管理您的项目！",
+  },
+  login: {
+    title: "从此进入，开始管理您的项目",
+    userName: "请输入用户名/手机号码",
+    password: "请输入密码",
+    loginBtn: "登录",
+    backBtn: "返回首页",
+    registerBtn: "先去注册",
+    passwordMinLength: "密码长度不能小于6位",
+  },
+  profile: {
+    groupName: "项目组",
+    start: "立项", // 立项 -- 意味没有定下一个版本之前
+    develop: "迭代中", // 迭代中 -- 确定版本号，开始开发了
+    maintaining: "维护中", // 维护中 -- 已有版本开发完，且没有开发下一个版本功能前
+    end: "终止", // 终止 -- 不再迭代和不再维护
+    status: "状态",
+    version: "版本号",
+    addGroup: "新增项目组",
+  },
+  HeaderComp: {
+    login: "登录",
+    logout: "退出登录",
+  },
 };
 
 export default ZhCN;
